4f2914c0f9f194a9cce125f9a952c147d16a5ef3
sammite <54330632+sammite@users.noreply.github.com>
2024-06-07 19:24:29 -0400

    Update hello_1.php

--- understandvulns/hello_1.php
+++ understandvulns/hello_1.php
@@ -1,11 +1,11 @@
 <?php
 // this is meant to be a very simple demonstration of how url parameters can be "hacked"
 
-echo "$user_val is user privilege level\n";
+
 if(isset($_GET['user_status'])) {
 
     $user_val = $_GET["user_status"];
-	
+	echo "$user_val is user privilege level\n";
 
         // Display a friendly message
     if ($user_val == "admin") {
