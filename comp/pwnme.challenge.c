d0dd7fac34476bbdf387c6238f3593cc9f399de0
sammite <54330632+sammite@users.noreply.github.com>
2024-06-08 10:48:01 -0400

    Create pwnme.challenge.c

---
+++ comp/pwnme.challenge.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char buffer[11]; // A buffer with a fixed size of 10 bytes
+    int protector = 0;
+    long long int specialval = 7;
+    printf("Enter some text: ");
+    gets(buffer); 
+
+
+    printf("You entered: %s\n", buffer);
+    printf("value of specialval: 0x%lx 0x%x\n", specialval, protector);
+    if (specialval == 0x6665656264616564 && protector == 0) {
+    	secretfunction();
+    } else {
+    	printf("I don't have a gatekey\n");
+    }
+    return 0;
+}
+
+
+int secretfunction() {
+	printf("Oh you mean this gatekey\nPCSC-BUFFER-BEEP \n\n");
+	return 0;
+}
