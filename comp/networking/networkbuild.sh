8ee6837ded66d0a3d919f3eba18c2c9dc9e76c5b
foo <foo@bar.com>
2024-06-12 21:24:26 -0400

    added some code to webapp

--- comp/networking/networkbuild.sh
+++ comp/networking/networkbuild.sh
@@ -5,7 +5,7 @@
 # pass a comms over a raw TCP connection
 
 
-printf "hello my name is PCSC-NETCAT-FLAG-PCSC" | nc 192.168.1.2
+printf "hello my name is PCSC-NETCAT-FLAG-PCSC" | nc 192.168.2.5 456
 
 
 # Telnet and connect to a sus telnet server
@@ -14,7 +14,7 @@ telnet 10.10.10.10
 
 # Pass a 7zipped file over UDP connection
 
-cat  file.7z | netcat localhost 8000 -w 1
+cat  file.7z | netcat 172.16.12.4 8000 -w 1
 
 
 # ncat -vl -p 8000 > file.foo
@@ -22,11 +22,11 @@ cat  file.7z | netcat localhost 8000 -w 1
 # Pass a Meme over the network connection
 #
 
-curl http://192.168.1.2:8080/meme.jpg
+curl http://10.10.10.4:8080/meme.png
 
 
 # Pass a nice cookie over the network with a PCSC flag
-curl --cookie "name=$(echo 'PCSC-COOKIE-FLAG-PCSC' | base64 )" google.com
+curl --cookie "name=$(echo 'PCSC-CAN-I-HAS-COOKIE-PCSC' | base64 )" google.com
 
 
 
